29fe32f0a480effb67e55ef76e98bfa14fbf9425
Clayton Carlson <claytonrc97@gmail.com>
2023-04-12 11:30:01 -0700

    core deliverables complete

--- server/app.py
+++ server/app.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
-from flask import Flask, request
+from flask import Flask, request, make_response, jsonify
 from flask_migrate import Migrate
+
 
 from models import db, Episode, Guest, Appearance
 
@@ -17,6 +18,56 @@ db.init_app(app)
 @app.route('/')
 def home():
     return ''
+
+@app.route('/episodes', methods=['GET'])
+def get_episodes():
+    episodes = Episode.query.all()
+    return jsonify([episode.to_dict() for episode in episodes])
+
+
+@app.route('/episodes/<int:id>', methods=['GET'])
+def get_episode(id):
+    episode = Episode.query.get(id)
+    if episode:
+        return jsonify(episode.to_dict())
+    else:
+        return jsonify({"error": "404: Episode not found"}), 404
+
+
+@app.route('/episodes/<int:id>', methods=['DELETE'])
+def delete_episode(id):
+    episode = Episode.query.get(id)
+    if episode:
+        db.session.delete(episode)
+        db.session.commit()
+        return make_response("", 204)
+    else:
+        return jsonify({"error": "404: Episode not found"}), 404
+
+
+@app.route('/guests', methods=['GET'])
+def get_guests():
+    guests = Guest.query.all()
+    return jsonify([guest.to_dict() for guest in guests])
+
+
+@app.route('/appearances', methods=['POST'])
+def create_appearance():
+    try:
+        rating = int(request.json['rating'])
+        episode_id = int(request.json['episode_id'])
+        guest_id = int(request.json['guest_id'])
+
+        appearance = Appearance(rating=rating, episode_id=episode_id, guest_id=guest_id)
+        db.session.add(appearance)
+        db.session.commit()
+
+        return jsonify(appearance.to_dict()), 201
+
+    except Exception as e:
+        return jsonify({"error": f"400: Validation error. {str(e)}"}), 400
+
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -1,6 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
-from sqlalchemy.orm import validates
+from sqlalchemy.orm import validates, relationship
 from sqlalchemy.ext.associationproxy import association_proxy
 from sqlalchemy_serializer import SerializerMixin
 
@@ -15,17 +15,46 @@ db = SQLAlchemy(metadata=metadata)
 
 class Episode(db.Model, SerializerMixin):
     __tablename__ = 'episodes'
+    serialize_rules = ('-appearances.episode', '-guests.appearances')
 
     id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.String)
+    number = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
-class Guest(db.Model, SerializerMixin):
-    __tablename__ = 'guests'
-
-    id = db.Column(db.Integer, primary_key=True)
+    appearances = db.relationship("Appearance", back_populates="episode")
+    guests = association_proxy("appearances", "guest")
 
 class Appearance(db.Model, SerializerMixin):
     __tablename__ = 'appearances'
+    serialize_rules = ('-episode.appearances', '-guest.appearances')
 
     id = db.Column(db.Integer, primary_key=True)
+    episode_id = db.Column(db.Integer, db.ForeignKey('episodes.id'))
+    guest_id = db.Column(db.Integer, db.ForeignKey('guests.id'))
+    rating = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
-# add any models you may need. 
+    episode = db.relationship("Episode", back_populates="appearances")
+    guest = db.relationship("Guest", back_populates="appearances")
+
+    @validates('rating')
+    def validate_rating(self, key, rating):
+        if not 1 <= rating <= 5:
+            raise ValueError('Rating must be between 1 and 5 (inclusive)')
+        return rating
+
+class Guest(db.Model, SerializerMixin):
+    __tablename__ = 'guests'
+    serialize_rules = ('-appearances.guest', '-episodes.appearances')
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    occupation = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    appearances = db.relationship("Appearance", back_populates="guest")
+    episodes = association_proxy("appearances", "episode")
